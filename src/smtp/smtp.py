4723b412097eb93d38f3f6135f18f6e1a1500f2d
Piotr Burchardt <pioburler@gmail.com>
2026-01-04 12:41:18 +0100

    no coverage dodany

--- src/smtp/smtp.py
+++ src/smtp/smtp.py
@@ -1,3 +1,3 @@
-class SMTPClient:
-    def send(subject, text, email_address) -> bool:
+class SMTPClient:  # pragma: no cover
+    def send(subject, text, email_address) -> bool:  # pragma: no cover
         return False
